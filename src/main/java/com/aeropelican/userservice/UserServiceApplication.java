09dd248f5aa60f67c7ed1ee9f3e4d725b2fa23c2
sairam goud <sairamgoud3108@gmail.com>
2026-09-02 11:29:18 +0530

    implemented user service

--- src/main/java/com/aeropelican/userservice/UserServiceApplication.java
+++ src/main/java/com/aeropelican/userservice/UserServiceApplication.java
@@ -2,11 +2,14 @@ package com.aeropelican.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
+
 }
