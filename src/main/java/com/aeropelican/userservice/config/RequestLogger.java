1fd52faad3e3a60e6ad22bf1cf07db24f840a309
Sowmya TG <soumya111tg@gmail.com>
2026-09-04 02:06:52 +0530

    New UI enhancements and API Upgrades

---
+++ src/main/java/com/aeropelican/userservice/config/RequestLogger.java
@@ -0,0 +1,26 @@
+package com.aeropelican.userservice.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class RequestLogger extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        log.info("Received a new request: {}", request.getRequestURI());
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/aeropelican/userservice/config/SecurityConfig.java
+++ src/main/java/com/aeropelican/userservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.aeropelican.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
@@ -48,6 +49,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/**", "/api/v1/users/auth/email/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
